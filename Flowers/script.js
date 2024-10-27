8bc557b13f8d34f5f5749bcf2e5dca4810338917
Fabrice2019 <fafalove2017@gmail.com>
2024-10-27 22:31:22 +0100

    individual project

---
+++ Flowers/script.js
@@ -0,0 +1,215 @@
+
+
+{
+    "@context": "https://schema.org/",
+    "@type": "WebSite",
+    "name": "Blumen verschicken mit Fleurop",
+    "url": "https://www.fleurop.de/",
+    "potentialAction": {
+        "@type": "SearchAction",
+        "target": "https://www.fleurop.de/search?search={search_term_string}",
+        "query-input": "required name=search_term_string"
+    }
+}
+
+
+
+                            
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Blumen verschicken mit Fleurop",
+    "url": "https://www.fleurop.de/",
+    "logo": "https://fleurop.cstatic.io/media/c2/38/4d/1676046536/fleurop.svg",
+    "contactPoint": {
+        "@type": "ContactPoint",
+                                    "email": "info@fleurop.de",
+        "contactType": "customer service",
+        "areaServed": "DE",
+        "availableLanguage": "German"
+    },
+    "sameAs": [""]
+}
+</script>
+
+
+                                       
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+(() => {
+    const analyticsStorageEnabled = document.cookie.split(';').some((item) => item.trim().includes('google-analytics-enabled=1'));
+    const adsEnabled = document.cookie.split(';').some((item) => item.trim().includes('google-ads-enabled=1'));
+
+    // Always set a default consent for consent mode v2
+    gtag('consent', 'default', {
+        'ad_user_data': adsEnabled ? 'granted' : 'denied',
+        'ad_storage': adsEnabled ? 'granted' : 'denied',
+        'ad_personalization': adsEnabled ? 'granted' : 'denied',
+        'analytics_storage': analyticsStorageEnabled ? 'granted' : 'denied'
+    });
+})();
+</script>
+
+
+  <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="f6ff4cd8-4144-4eea-a205-9551a8fd2c48" type="text/javascript" async></script>
+            
+            <script>
+window.features = {"V6_5_0_0":true,"v6.5.0.0":true,"V6_6_0_0":false,"v6.6.0.0":false,"V6_7_0_0":false,"v6.7.0.0":false,"VUE3":false,"vue3":false,"JOIN_FILTER":false,"join.filter":false,"MEDIA_PATH":false,"media.path":false,"ES_MULTILINGUAL_INDEX":false,"es.multilingual.index":false,"STOCK_HANDLING":false,"stock.handling":false,"ASYNC_THEME_COMPILATION":false,"async.theme.compilation":false};
+</script>
+
+
+
+                                <script>
+                window.activeNavigationId = 'b863f58948b74fb088aea4b5c148d5a2';
+                window.router = {
+                    'frontend.cart.offcanvas': '/checkout/offcanvas',
+                    'frontend.cookie.offcanvas': '/cookie/offcanvas',
+                    'frontend.checkout.finish.page': '/checkout/finish',
+                    'frontend.checkout.info': '/widgets/checkout/info',
+                    'frontend.menu.offcanvas': '/widgets/menu/offcanvas',
+                    'frontend.cms.page': '/widgets/cms',
+                    'frontend.cms.navigation.page': '/widgets/cms/navigation',
+                    'frontend.account.addressbook': '/widgets/account/address-book',
+                    'frontend.country.country-data': '/country/country-state-data',
+                    'frontend.app-system.generate-token': '/app-system/Placeholder/generate-token',
+                    };
+                window.salesChannelId = '947c7348c04a4c1c91737803bed59f42';
+            </script>
+
+
+
+
+                                <script>
+                
+                window.breakpoints = {"xs":0,"sm":576,"md":768,"lg":992,"xl":1200};
+            </script>
+        
+                                    <script>
+                    window.customerLoggedInState = 0;
+
+                    window.wishlistEnabled = 1;
+                </script>
+                    
+                    <script src="https://www.fleurop.de/bundles/netzpshariff6/jquery-3.5.1.slim.min.js?1727363954"></script>
+
+                            <script>
+                window.themeAssetsPublicPath = 'https://www.fleurop.de/theme/1d07b69c80ca48188d9beeb81dd2fc17/assets/';
+            </script>
+
+
+                                          <script type="text/javascript" src="https://www.fleurop.de/theme/3c2a7dcaa1e2367fd8dfd2caad069e0d/js/all.js?1727363966" defer></script>
+
+
+
+
+         
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org/",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+                                            {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Alle Sträuße",
+                "item": "https://www.fleurop.de/alle-blumenstraeusse"
+            }                            ]
+}
+</script>
+
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": [{
+    "@type": "Question",
+    "name": "Welche Blumen eignen sich im Blumenstrauß?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Die Rose ist immer modern und gilt vor allem in der Farbe Rot als die Blume der Liebe und findet sich deshalb häufig in Sträußen zum Valentinstag oder als Liebesgruß. Auch bunte Rosen sind ein liebevolles Geschenk für Freunde und Verwandte. Beliebt für Blumensträuße mit und ohne Grußkarte sind außerdem Gerbera und Tulpen, weil diese Blumen eine besonders bunte Farbvielfalt bieten. Im Sommer peppen Sie den Strauß mit Sonnenblumen auf. Auch Lilien, Gladiolen oder Ranunkeln machen sich gut in Blumensträußen. Unsere Partnerfloristen stellen Ihnen zu jeder Jahreszeit saisonale Blumensträuße mit und ohne Grün zusammen, die das Geburtstagskind oder Sie selbst erfreuen."
+    }
+  },{
+    "@type": "Question",
+    "name": "Wie hält mein Blumenstrauß länger?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Damit Sie besonders lange Freude an Ihrem Blumenstrauß haben, liefern wir mit jeder Bestellung ein Päckchen Frischhaltemittel. Dieses wird einfach zum Blumenwasser hinzugefügt und sorgt dafür, dass die Blumen lange frisch bleiben."
+    }
+  },{
+    "@type": "Question",
+    "name": "Was kostet ein Fleurop Blumenstrauß?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Bei Fleurop finden Sie online Blumensträuße in verschiedenen Preiskategorien, die für jedes Budget mehrere Sträuße zur Auswahl bereithalten. Sie können bequem nach dem passenden Preis filtern, um den richtigen Strauß für den anstehenden Anlass zu finden. Dabei beginnen unsere Blumenarrangements bei unter 20 Euro und reichen bis über 100 Euro.
+Hinzu kommen die Versandkosten je nach Land. Die Nutzung des Lieferservices am Sonntag ist dabei für einen kleinen Aufpreis möglich. So erreichen Ihre Blumengrüße den Empfänger immer zum richtigen Zeitpunkt."
+    }
+  },{
+    "@type": "Question",
+    "name": "Wie groß sind Fleurop Sträuße?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Bei der Bestellung der Blumensträuße bei Fleurop haben Sie die Möglichkeit, aus drei verschiedenen Größen zu wählen - von M bis XXL. Sie finden die Größenauswahl beim jeweiligen Blumenstrauß und können hier die verschiedenen Preisstufen einsehen. Bei einigen ausgewählten Sträußen haben Sie zudem die Möglichkeit, über den Preis Ihre individuelle Wunschgröße zu definieren."
+    }
+  },{
+    "@type": "Question",
+    "name": "Wer liefert bei Fleurop?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Die Fleurop-Blumensträuße werden mit viel Liebe von Floristen zusammengestellt und gebunden. Die Lieferung der Blumen erfolgt danach durch unsere Fleurop-Boten an die beschenkte Person. Zusätzlich liefern wir ausgewählte Blumenarrangements und Geschenke per Versandunternehmen und arbeiten dabei mit DHL zusammen."
+    }
+  },{
+    "@type": "Question",
+    "name": "Welcher Blumenladen bietet Fleurop an?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Mit unserer unkomplizierten Floristensuche finden Sie schnell einen passenden Blumenladen in Ihrer Nähe, mit dem Sie Fleurop-Blumensträuße verschicken können. Geben Sie einfach Ihre Postleitzahl ein und finden Sie das passende Blumenfachgeschäft für Ihre nächste Fleurop-Bestellung."
+    }
+  }]
+}
+</script>
+
+
+ <script type="text/javascript">
+            (function () {
+                const _tsid = 'X43AA527C7FAF2848EBC11CF0EB0DC08F';
+                _tsConfig = {
+                    'yOffset': '0', /* offset from page bottom */
+                    'variant': 'reviews', /* default, reviews, custom, custom_reviews */
+                    'customElementId': '', /* required for variants custom and custom_reviews */
+                    'trustcardDirection': '', /* for custom variants: topRight, topLeft, bottomRight, bottomLeft */
+                    'customBadgeWidth': '', /* for custom variants: 40 - 90 (in pixels) */
+                    'customBadgeHeight': '', /* for custom variants: 40 - 90 (in pixels) */
+                    'disableResponsive': 'false', /* deactivate responsive behaviour */
+                    'disableTrustbadge': 'false' /* deactivate trustbadge */
+                };
+                const _ts = document.createElement('script');
+                _ts.type = 'text/javascript';
+                _ts.charset = 'utf-8';
+                _ts.async = true;
+                _ts.src = '//widgets.trustedshops.com/js/' + _tsid + '.js';
+                const __ts = document.getElementsByTagName('script')[0];
+                __ts.parentNode.insertBefore(_ts, __ts);
+            })();
+        </script>
+
+
+<script>
+        var custobarEnabled = document.cookie.split('; ')
+            .find(row => (row == 'custobar-enabled=1'));
+
+        if (custobarEnabled) {
+            (function(c,u,s,t,o,b,a,r) {var e;c[o]=[];c[b]=a;e=u.createElement(s);
+                r=u.getElementsByTagName(s)[0];e.async=1;e.src=t;
+                r.parentNode.insertBefore(e,r);})(window,document,'script',
+                'https://api.custobar.com/js/v1/custobar.js',
+                'cstbr','cstbrConfig',{'_companyToken':'CBRGZMK5HEOKZERP6BU7DYAYOPOHHJT5DUJPEENP'});
+
+                                }
+    </script>
+
+
+
